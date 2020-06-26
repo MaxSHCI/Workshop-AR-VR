ab7e09e25dc87a1f82782eb16ece96445c51d2c8
MaxSHCI <65767794+MaxSHCI@users.noreply.github.com>
2020-06-26 20:13:25 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -3,7 +3,7 @@ var myOtherBox = document.getElementById('myOtherBox');
 
 function bounce(){
 myOtherBox.object3D.position.y += rotationSpeed;
-console.log(myOtherBox.object3D.rotation.x);
+console.log(myOtherBox.object3D.position.x);
 }
 
 setInterval(spin, 16); //equivalent to 60 fps
