fe9ebdd01efb5309c2e2a3604b063d0c12993003
MaxSHCI <65767794+MaxSHCI@users.noreply.github.com>
2020-06-26 20:12:32 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -2,7 +2,7 @@ var rotationSpeed = 1.00
 var myOtherBox = document.getElementById('myOtherBox');
 
 function bounce(){
-myOtherBox.object3D.rotation.x += rotationSpeed;
+myOtherBox.object3D.position.y += rotationSpeed;
 console.log(myOtherBox.object3D.rotation.x);
 }
 
